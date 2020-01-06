22173bcf5759c008e522cd7e8a7bd969cc999ef7
Rob Wilson <roobert@gmail.com>
2020-01-06 16:38:49 +0000

    refactor

--- variable_updater/__main__.py
+++ variable_updater/__main__.py
@@ -4,53 +4,16 @@ import sys
 from urllib3.exceptions import ProtocolError
 from requests.exceptions import ConnectionError, HTTPError, RequestException
 
-from .cli import parse_args, environment_variables, EnvironmentVariableError
-from .config_file import ConfigFileError, variables
-from .vault import Vault, VaultKeyError, VaultServerError, VaultAuthError
-from .bitbucket_requester import BitBucketRequester, BitBucketGetError
-from .bitbucket_variable import BitBucketVariable
+from .cli import EnvironmentVariableError
+from .config_file import ConfigFileError
+from .vault import VaultKeyError, VaultServerError, VaultAuthError
+from .bitbucket_requester import BitBucketGetError
+from .variable_updater import variable_updater
 
 
 def main():
-    args = parse_args()
-    config = environment_variables()
-
-    vault = Vault(
-        server=config.server, username=config.username, password=config.password
-    )
-
-    bitbucket_username = vault.read(
-        config.bitbucket_key_mount, config.bitbucket_username_key
-    )
-    bitbucket_password = vault.read(
-        config.bitbucket_key_mount, config.bitbucket_password_key
-    )
-
-    # NOTE: requires a bitbucket "app password" with edit variables permissions
-    requester = BitBucketRequester(
-        username=bitbucket_username, password=bitbucket_password,
-    )
-
-    for variable in variables(args.config):
-        value = vault.read(variable["vault_mount"], variable["value"])
-
-        try:
-            variable = BitBucketVariable(
-                requester=requester,
-                workspace=variable["workspace"],
-                repo=variable["repo"],
-                key=variable["key"],
-                value=value,
-            )
-        except AttributeError as error:
-            raise ConfigFileError(f"key failure for config file: {error}")
-
-        variable.upsert()
-
-
-if __name__ == "__main__":
     try:
-        main()
+        variable_updater()
     except (
         ConfigFileError,
         EnvironmentVariableError,
@@ -64,6 +27,11 @@ if __name__ == "__main__":
         HTTPError,
         OSError,
         RequestException,
+        Exception,
     ) as error:
-        print(f"{type(error)}: {error}")
+        print(f"{error.__class__.__name__}: {error}")
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ variable_updater/variable_updater.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from .cli import parse_args, environment_variables
+from .config_file import variables, ConfigFileError
+from .vault import Vault
+from .bitbucket_requester import BitBucketRequester
+from .bitbucket_variable import BitBucketVariable
+
+
+def variable_updater():
+    args = parse_args()
+    config = environment_variables()
+
+    vault = Vault(
+        server=config.server, username=config.username, password=config.password
+    )
+
+    bitbucket_username = vault.read(
+        config.bitbucket_key_mount, config.bitbucket_username_key
+    )
+    bitbucket_password = vault.read(
+        config.bitbucket_key_mount, config.bitbucket_password_key
+    )
+
+    # NOTE: requires a bitbucket "app password" with edit variables permissions
+    requester = BitBucketRequester(
+        username=bitbucket_username, password=bitbucket_password,
+    )
+
+    for variable in variables(args.config):
+        value = vault.read(variable["vault_mount"], variable["value"])
+
+        try:
+            variable = BitBucketVariable(
+                requester=requester,
+                workspace=variable["workspace"],
+                repo=variable["repo"],
+                key=variable["key"],
+                value=value,
+            )
+        except AttributeError as error:
+            raise ConfigFileError(f"key failure for config file: {error}")
+
+        variable.upsert()
